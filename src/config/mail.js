e5378a6b983e3c9b96e29f684ee7f51a9c6378ff
Adalberto Ferreira <adalbertoferreiradasilvaneto@gmail.com>
2020-02-27 15:36:11 -0300

    Adicionada dependência do nodemailer e lógica de envio de e-mails ao prestador ao cancelar um agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b5df41a4313c51',
+    pass: '4dcfe4c8726618',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
